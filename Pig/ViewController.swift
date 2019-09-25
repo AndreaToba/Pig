988c8145e2a62e02d4e778dc786163750be69768
Andrea Toba <andrea.i.toba@gmail.com>
2019-09-25 14:23:26 -0500

    Change in layout

--- Pig/ViewController.swift
+++ Pig/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var turnScore: UILabel!
+    @IBOutlet weak var score1: UILabel!
+    @IBOutlet weak var score2: UILabel!
+    @IBOutlet weak var playerTurnLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func endTurn(_ sender: Any) {
+    }
+    
+    @IBAction func roll(_ sender: Any) {
+    }
 }
 
