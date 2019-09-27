3d1ebb38c5cb12a41850e856b0642a7249d85e32
Megha Arya <megha.arya07@gmail.com>
2019-09-27 14:20:08 -0500

    commentated out rotate function

--- Pig/ViewController.swift
+++ Pig/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func roll(_ sender: Any) {
+     //   UIImage.rotate(180)
         let randomInt = Int.random(in: 1..<6)
         print(randomInt)
         if(randomInt>0){
@@ -36,5 +37,32 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    
+   
 }
+
+
+/*extension UIImage {
+    func rotate(radians: CGFloat) -> UIImage {
+        let rotatedSize = CGRect(origin: .zero, size: size)
+            .applying(CGAffineTransform(rotationAngle: CGFloat(radians)))
+            .integral.size
+        UIGraphicsBeginImageContext(rotatedSize)
+        if let context = UIGraphicsGetCurrentContext() {
+            let origin = CGPoint(x: rotatedSize.width / 2.0,
+                                 y: rotatedSize.height / 2.0)
+            context.translateBy(x: origin.x, y: origin.y)
+            context.rotate(by: radians)
+            draw(in: CGRect(x: -origin.y, y: -origin.x,
+                            width: size.width, height: size.height))
+            let rotatedImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            return rotatedImage ?? self
+        }
+        
+        return self
+    }
+ }*/
 
