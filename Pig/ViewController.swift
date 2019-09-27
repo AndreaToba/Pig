f1fa0b9f0ac0d533a3139275fbd3d6d4112fe9a5
Andrea Toba <andrea.i.toba@gmail.com>
2019-09-27 14:21:20 -0500

    variables and base logic

--- Pig/ViewController.swift
+++ Pig/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var score2: UILabel!
     @IBOutlet weak var playerTurnLabel: UILabel!
     @IBOutlet var dieImage: UIImageView!
+    var player1Turn: Bool = true
+    var player1Score: Int = 0
+    var player2Score: Int = 0
+    var playerTurnScore: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +26,31 @@ class ViewController: UIViewController {
     }
 
     @IBAction func endTurn(_ sender: Any) {
+        turnToTotal()
     }
     
     @IBAction func roll(_ sender: Any) {
         let randomInt = Int.random(in: 0..<6)
         dieImage.image=UIImage(named: "face\(randomInt)")
         
-        
+        if (randomInt == 1){
+            playerTurnScore = 0
+            turnToTotal()
+        }
+        else{
+            playerTurnScore += randomInt
+        }
+     }
+    
+    func turnToTotal(){
+        if (player1Turn){
+            player1Score += playerTurnScore
+        }
+        else{
+            player2Score += playerTurnScore
+        }
+        player1Turn = !player1Turn
     }
+    
 }
 
